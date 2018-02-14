34f682d1d25663197ae0babbeffba78c686bf8dd
mcardillo <mcardillo@eagles.bridgewater.edu>
2018-02-14 15:29:11 -0500

    Update listClubsByRating.php
    Joined colleges table and added college name.

--- Code/PHP/listClubsByRating.php
+++ Code/PHP/listClubsByRating.php
@@ -1,13 +1,13 @@
 <?php
 require_once('DBconnect.php');
 
-$sql = "SELECT club_name, avg_club_rating FROM clubs ORDER BY avg_club_rating LIMIT 10";
+$sql = "SELECT clubs.club_name, clubs.avg_club_rating, colleges.name FROM clubs INNER JOIN colleges ON clubs.collegeID=colleges.collegeID ORDER BY avg_club_rating LIMIT 10";
 
 $result = $dbc->query($sql);
 
 if($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
-		echo "<li>". $row["club_name"]. " &#9 ". $row["avg_club_rating"]. "</li>";
+		echo "<li>". $row["clubs.club_name"]. " &#9 ". $row["colleges.name"]. " &#9 " . $row["clubs.avg_club_rating"]. "</li>";
 	}
 else {
 	echo "Not found.";
