1639e78ac747fa8bbe649e2b0b5b8e609abab21c
mcardillo <mcardillo@eagles.bridgewater.edu>
2018-02-25 22:55:27 -0500

    Update listClubsByRating.php
    Fixed and tested SQL statement.

--- Code/PHP/listClubsByRating.php
+++ Code/PHP/listClubsByRating.php
@@ -1,18 +1,54 @@
 <?php
+
 require_once('DBconnect.php');
 
-$sql = "SELECT clubs.club_name, clubs.avg_club_rating, colleges.name FROM clubs INNER JOIN colleges ON clubs.collegeID=colleges.collegeID ORDER BY avg_club_rating LIMIT 10";
+echo "made it to list clubs";
 
-$result = $dbc->query($sql);
+//THIS STATEMENT HAS BEEN TESTED AND WORKS
+//$sql = "SELECT Clubs.club_name, Clubs.club_avg_rating, Colleges.college_name FROM Clubs INNER JOIN Colleges ON Clubs.college_id=Colleges.college_id ORDER BY club_avg_rating DESC LIMIT 10";
+
+//$sql = "SELECT * FROM clubs";
+
+$dbc = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+
+    OR die('Could not connect to MySQL: ' . mysqli_connect_error());
+
+$emailquery = "SELECT * FROM clubs;";
+
+$emailreponse = $dbc->query($emailquery);
+
+print_r($emailresponse);
+
+
+
+// TODO, none of this is currently working
+
+
+
+
+
+/*$result = $dbc->query($sql);
+
+echo 'printing';
+
+print_r($result);
+
+echo 'printed';
 
 if($result->num_rows > 0) {
-	while($row = $result->fetch_assoc()) {
-		echo "<li>". $row["clubs.club_name"]. " &#9 ". $row["colleges.name"]. " &#9 " . $row["clubs.avg_club_rating"]. "</li>";
-	}
+
+    while($row = $result->fetch_assoc()) {
+
+        echo "<li>". $row["clubs.club_name"]. " &#9 ". $row["colleges.name"]. " &#9 " . $row["clubs.avg_club_rating"]. "</li>";
+
+    }
+
 else {
-	echo "Not found.";
-}
 
-$dbc->close();
+        echo "Not found.";
+
+    }
+
+    $dbc->close();*/
+
 ?>
-
